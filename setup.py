2bfddeba53426e18dd3d2a779879917a784f34f8
liuf5 <liuf5@knownsec.com>
2021-01-13 23:18:46 +0800

    fix:update install_requirs,add long decription

--- setup.py
+++ setup.py
@@ -11,17 +11,22 @@
 """
 from setuptools import setup
 
+
+DEPENDENCIES = open('requirements.txt', 'r').read().split('\n')
+README = open('README.md', 'r').read()
+
 setup(
-    name='zoomeye',
+    name='zoomeye-python',
     version='0.3',
-    description='Python library and command-line utility for ZoomEye (https://www.zoomeye.org/doc)',
-    long_description=open('README.md').read(),
+    description='Python library and command-line tool for ZoomEye (https://www.zoomeye.org/doc)',
+    long_description=README,
     long_description_content_type='text/x-rst',
     author='404 Team@Knownsec',
+    url='https://github.com/knownsec/zoomeye-python',
     packages=['zoomeye'],
     entry_points={'console_scripts': ['zoomeye=zoomeye.cli:main']},
-    install_requires="DEPENDENCIES",
-    keywords=['security tool', 'zoomeye'],
+    install_requires=DEPENDENCIES,
+    keywords=['security tool', 'zoomeye', 'command tool'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -29,6 +34,11 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
